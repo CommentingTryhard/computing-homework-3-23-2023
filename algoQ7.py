dfc6ac3805b17c752a5dc284e3fe784c2703af66
CommentingTryhard <88810104+CommentingTryhard@users.noreply.github.com>
2023-03-26 17:32:16 +0800

    Create algoQ7.py

---
+++ algoQ7.py
@@ -0,0 +1,8 @@
+bears = int(input("pls put how many bearbear u make: "))
+hours = int(input("pls put how many hours u work: "))
+
+money_bear = bears*2
+money_hour = hours*5
+
+yo = max(money_bear, money_hour)
+print("$ {0} is how much money u mak.".format(yo))
