d0e0589a6a868cfa9826721b0e7b74ad972acf36
CommentingTryhard <88810104+CommentingTryhard@users.noreply.github.com>
2023-03-26 17:06:05 +0800

    Create algoQ4.py

---
+++ algoQ4.py
@@ -0,0 +1,9 @@
+dogage = int(input("wat is the age of yer doggo: "))
+if dogage <= 2:
+  hooman = dogage*12
+  print("human age: {0} ".format(hooman)
+
+else:
+        hooman = 24+((dogage-2)*6)
+        print("human age: {0} ".format(hooman)
+       
