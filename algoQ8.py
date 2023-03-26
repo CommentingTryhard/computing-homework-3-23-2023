5448627ea4dd1fac0d5bd904ea94edfa45877003
CommentingTryhard <88810104+CommentingTryhard@users.noreply.github.com>
2023-03-26 17:35:59 +0800

    Create algoQ8.py

---
+++ algoQ8.py
@@ -0,0 +1,7 @@
+lengthy = int(input("Enter length 1: "))
+length = int(input("Enter length 2: "))
+if lengthy == length:
+  print("triangle is isoceles")
+else:
+  print("triangle is not isoceles")
+ 
